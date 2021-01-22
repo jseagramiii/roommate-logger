10e9ae6482314e2bc5f08edfa5e20c580665e6dc
James Seagram <jseagramiii@gmail.com>
2021-01-22 14:24:36 -0700

    deploying

--- server.js
+++ server.js
@@ -5,14 +5,13 @@ const path = require('path')
 
 // App configuration
 const app = express()
-const PORT = process.env.PORT || 5000
 const connection_url =
   'mongodb+srv://admin:iTAw1W5orjFaSCz7@cluster0.rkovu.mongodb.net/roommateloggerdb?retryWrites=true&w=majority'
+// Connect DB
+connectDB()
 
 // Middleware
 app.use(express.json({ extended: false }))
-// Connect DB
-connectDB()
 
 // Routes
 app.use('/api/users', require('./routes/users'))
@@ -27,6 +26,8 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   )
 }
+
+const PORT = process.env.PORT || 5000
 
 // Listener
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
